b3cd4372336a589902f8fd35ab643989689a2516
Estian <estian.rosslee@gmail.com>
2022-03-04 17:37:08 +0200

    Initial work on new puzzle generation
    Implemented ability to generate new solvable puzzles.

--- sudoku.py
+++ sudoku.py
@@ -1,4 +1,23 @@
 import copy
+import random
+from hashlib import new
+from re import T
+
+
+def create_empty_board():
+    # create 9*9 grid populated with 0
+    board = [
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0,0],
+    ]
+    return board
 
 def print_board(board, title="Sudoku:"):
     top = "_"*27
@@ -56,13 +75,69 @@ def is_puzzle_solvable(board, brute_force=False):
         solvable = generate_solution(board_copy)
         return solvable
 
-def generate_sudoku_puzzle(board):
+def generate_new_complete_sudoku_puzzle(board):
     # use backtracking to generate a solvable sudoku puzzle
     # game_board is a list of lists of size 9**2
     # each inner list represents a row in the game board
     # valid values: 1-9, unfilled/available spaces: 0
     # minimum number of clues to guarantee solution is 17
-    pass
+
+    valid_values = [1,2,3,4,5,6,7,8,9]
+    random.shuffle(valid_values) # shuffle to try in possible values in random order
+
+    unfilled_space = find_next_unfilled(board)
+    if unfilled_space[0] == -1:
+        return True
+    
+    for value in valid_values:
+        row, col = unfilled_space
+
+        if is_value_valid_for_space(board, value, unfilled_space):
+            board[row][col] = value
+            if generate_new_complete_sudoku_puzzle(board):
+                return True
+        else:
+            board[row][col] = 0
+    
+    return False
+
+def remove_numbers_from_board(board, clues_left=17):
+    num_to_remove = 81-clues_left
+
+    solvable = False
+    while not solvable:
+        board_copy = copy.deepcopy(board)
+
+        # create mask
+        mask = []
+        while len(mask) < num_to_remove:
+            i = random.randint(0,80)
+            if i not in mask:
+                mask.append(i)
+
+        # apply mask
+        for i in mask:
+            row=i // 9
+            col=i % 9
+            board_copy[row][col] = 0
+
+        solvable = is_puzzle_solvable(board=board_copy, brute_force=True)
+    
+    board = board_copy
+    return board
+
+def generate_new_sudoku_puzzle(board):
+    generated = False
+    while not generated:
+        generate_new_complete_sudoku_puzzle(board)
+        solvable = is_puzzle_solvable(board=board)
+        if solvable:
+            generated = True
+    print("generated")
+
+    board = remove_numbers_from_board(board, clues_left=27)
+    print("removed numbers")
+    return board
 
 def find_next_unfilled(board):
     # find and return (row, col) for first unfilled (val==0) board space
@@ -116,9 +191,7 @@ def generate_solution(board):
             board[space[0]][space[1]] = value
 
             # use recursion to brute force the search space
-            solved = generate_solution(board)
-
-            if solved:
+            if generate_solution(board):
                 return True
         elif board[space[0]][space[1]] > 0:
             board[space[0]][space[1]] = 0
@@ -126,21 +199,28 @@ def generate_solution(board):
     # if recursive brute force fails return False
     return False
 
-if __name__ == '__main__':
-    from puzzles.example_puzzle_easy import puzzle as easy
-    from puzzles.example_unsolvable import puzzle as unsolvable
-
-    puzzles = [easy, unsolvable]
-
-    for puzzle in puzzles:
-        print_board(puzzle, title="Puzzle:")
-        solvable = is_puzzle_solvable(board=puzzle)
-        if solvable:
-            solved = generate_solution(board=puzzle)
-            if solved:
-                print("Sudoku solved!!")
-                print_board(puzzle, title="Solution:")
-            else:
-                print("Uh Oh!! The puzzle is unsolvable")
+def attempt_puzzle(board):
+    print_board(board, title="Puzzle:")
+    solvable = is_puzzle_solvable(board=board)
+    if solvable:
+        solved = generate_solution(board=board)
+        if solved:
+            print("Sudoku solved!!")
+            print_board(board, title="Solution:")
         else:
-            print("The puzzle is unsolvable")
+            print("Uh Oh!! The puzzle is unsolvable")
+    else:
+        print("The puzzle is unsolvable")
+
+if __name__ == '__main__':
+    # from puzzles.example_puzzle_easy import puzzle as easy
+    # from puzzles.example_unsolvable import puzzle as unsolvable
+
+    # puzzles = [easy, unsolvable]
+
+    # for puzzle in puzzles:
+    #     attempt_puzzle(puzzle)
+
+    new_board = create_empty_board()
+    new_board = generate_new_sudoku_puzzle(board=new_board)
+    attempt_puzzle(new_board)
